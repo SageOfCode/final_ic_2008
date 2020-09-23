ef37cb5dff2e7058dcf1337c739f9bd2cc2eeedf
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-23 09:39:49 -0600

    Cookbook class passes it can add recipes test

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,3 +1,10 @@
 class CookBook
+  attr_reader :recipes
+  def initialize
+    @recipes = []
+  end
 
-end 
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
+end
--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -10,4 +10,13 @@ class CookbookTest < Minitest::Test
     cookbook = CookBook.new
     assert_instance_of CookBook, cookbook
   end
+
+  def test_can_add_recipes
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
