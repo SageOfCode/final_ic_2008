8f2c413cfee36394c8c7703d4ed971628aa5dcd7
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-23 09:54:17 -0600

    Passes it knows ingredient names test

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,12 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    @recipes.map do |recipe|
+      recipe.ingredients_required.map do |ingredient, count|
+        ingredient.name
+      end
+    end.flatten.uniq
+  end
 end
