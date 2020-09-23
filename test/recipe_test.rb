30c6c13dd631bbfbcab2a68964e119e5d2700a92
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-23 09:32:08 -0600

    Recipe class passes it can require ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -10,4 +10,17 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, recipe1
     assert_equal "Mac and Cheese", recipe1.name
   end
+
+  def test_it_can_require_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    expected1 = {}
+    assert_equal expected1, recipe1.ingredients_required
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected2 = {ingredient1 => 6, ingredient2 => 8}
+    assert_equal expected2, recipe1.ingredients_required
+  end
 end
